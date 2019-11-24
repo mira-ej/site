5fab95ee9509aaf966748e7a59edc0b42dd5750c
Gabriel Chaves <gabrielmoreirachaves@outlook.com>
2019-11-24 18:35:19 -0300

    Preloader added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+    var style = document.createElement("style");
+
+    document.head.appendChild(style);
+    style.sheet.insertRule(
+      "#preloader { clip-path: polygon(" +
+        "0% 0%, 0% 100%, 50% 100%, -200vmax 200vmax, 50% -100vmax," +
+        "300vmax 200vmax, -200vmax 200vmax, 50% 100%, 100% 100%, 100% 0%) }"
+    );
+
+    setTimeout(function() {
+      style.sheet.insertRule("body { overflow: unset }");
+      document.getElementById("preloader").remove();
+    }, 1500);
+  }, 2000);
+
   document.getElementById("scroll-down").addEventListener("click", function() {
     var about = document.getElementById("about");
     window.scroll(0, about.offsetTop);
